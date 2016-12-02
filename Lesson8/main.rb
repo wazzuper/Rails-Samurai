760231e0ff4bbe324ce345ab7693387bdd7adb10
artem <clifredd@gmail.com>
2016-12-02 18:11:38 +0500

    Lesson 8

---
+++ Lesson8/main.rb
@@ -0,0 +1,234 @@
+require_relative 'station'
+require_relative 'route'
+require_relative 'train'
+require_relative 'cargo_train'
+require_relative 'passenger_train'
+require_relative 'wagon'
+require_relative 'cargo_wagon'
+require_relative 'passenger_wagon'
+
+class Menu
+  def initialize
+    @stations = []
+    @trains = []
+  end
+
+  def menu_loop
+    loop do
+      puts 'Выберите пункт в меню:'
+      puts '1. Создать станцию'
+      puts '2. Создать поезд'
+      puts '3. Прицепить вагон к поезду'
+      puts '4. Отцепить вагон от поезда'
+      puts '5. Поместить поезд на станцию'
+      puts '6. Показать список станций'
+      puts '7. Показать список поездов на станции'
+      puts '8. Показать список вагонов поездов'
+      puts '9. Занять место или объем в вагоне'
+      puts '10. Выход'
+      menu = gets.chomp.to_i
+      case menu
+      when 1
+        add_station
+      when 2
+        add_train
+      when 3
+        add_wagon
+      when 4
+        remove_wagon
+      when 5
+        train_to_station
+      when 6
+        show_stations
+      when 7
+        show_trains
+      when 8
+        show_wagons
+      when 9
+        take_seat_or_volume
+      when 10
+        break
+      else
+        puts 'Введите число!'
+      end
+    end
+  end
+
+  private
+
+  def add_station
+    print 'Введите название станции: '
+    station = gets.chomp
+    @stations << Station.new(station)
+  rescue RuntimeError => e
+    puts e.to_s
+    retry
+  end
+
+  def add_train
+    print 'Укажите номер поезда: '
+    number = gets.chomp.to_s
+    print "Поезд будет 'Пассажирский' или 'Грузовой'? "
+    type = gets.chomp
+    if type == 'Пассажирский'
+      @trains << PassengerTrain.new(number, type)
+    elsif type == 'Грузовой'
+      @trains << CargoTrain.new(number, type)
+    else
+      puts 'Неверный тип поезда!'
+    end
+  rescue RuntimeError => e
+    puts e.to_s
+    retry
+  end
+
+  def add_wagon
+    if !@trains.empty?
+      puts 'Выберите поезд'
+      index = 0
+      @trains.each { |value| puts "#{index += 1} - #{value.number} - #{value.type}" }
+      choice = gets.chomp.to_i
+      if choice <= @trains.size
+        if @trains[choice - 1].type == 'Грузовой'
+          print 'Укажите объем вагона: '
+          volume = gets.chomp.to_i
+          @trains[choice - 1].add_wagon(CargoWagon.new(volume))
+        elsif @trains[choice - 1].type == 'Пассажирский'
+          print 'Укажите общее количество мест в вагоне: '
+          seats = gets.chomp.to_i
+          @trains[choice - 1].add_wagon(PassengerWagon.new(seats))
+        end
+      else
+        puts 'Нет такого поезда'
+      end
+    else
+      puts 'Сначала создайте поезд'
+    end
+  end
+
+  def remove_wagon
+    if !@trains.empty?
+      puts 'Выберите поезд'
+      index = 0
+      @trains.each { |value| puts "#{index += 1} - #{value.number} - #{value.type}" }
+      choice = gets.chomp.to_i
+      if choice <= @trains.size
+        @trains[choice - 1].remove_wagon
+      else
+        puts 'Нет такого поезда'
+      end
+    else
+      puts 'Сначала создайте поезд'
+    end
+  end
+
+  def train_to_station
+    if !@stations.empty? && !@trains.empty?
+      puts 'Выберите станцию'
+      index = 0
+      @stations.each { |value| puts "#{index += 1} - #{value.name}" }
+      choice = gets.chomp.to_i
+      if choice <= @stations.size
+        puts 'Выберите поезд'
+        index = 0
+        @trains.each { |value| puts "#{index += 1} - #{value.number} - #{value.type}" }
+        train_choice = gets.chomp.to_i
+        if train_choice <= @trains.size
+          @stations[choice - 1].get_train(@trains[train_choice - 1])
+        else
+          puts 'Нет такого поезда'
+        end
+      else
+        puts 'Нет такой станции'
+      end
+    elsif @stations.empty?
+      puts 'Сначала создайте станцию'
+    elsif @trains.empty?
+      puts 'Сначала создайте поезд'
+    end
+  end
+
+  def show_stations
+    puts 'Список станций:'
+    @stations.each { |value| puts value.name }
+  end
+
+  def show_trains
+    if !@stations.empty?
+      puts 'Выберите станцию'
+      index = 0
+      @stations.each { |value| puts "#{index += 1} - #{value.name}" }
+      choice = gets.chomp.to_i
+      if choice <= @stations.size
+        @stations[choice - 1].show { |train| puts "#{train.number} - #{train.type}" }
+      else
+        puts 'Нет такой станции'
+      end
+    else
+      puts 'Сначала создайте станцию'
+    end
+  end
+
+  def show_wagons
+    if !@trains.empty?
+      puts 'Выберите поезд'
+      index = 0
+      @trains.each { |value| puts "#{index += 1} - #{value.number} - #{value.type}" }
+      choice = gets.chomp.to_i
+      if choice <= @trains.size
+        index = 0
+        if @trains[choice - 1].type == 'Грузовой'
+          @trains[choice - 1].show_wagons { |wagon| puts "#{index += 1} - #{wagon.type} вагон - объем: #{wagon.volume}" }
+        elsif @trains[choice - 1].type == 'Пассажирский'
+          @trains[choice - 1].show_wagons { |wagon| puts "#{index += 1} - #{wagon.type} вагон - мест: #{wagon.volume}" }
+        end
+      else
+        puts 'Нет такого поезда'
+      end
+    else
+      puts 'Сначала создайте поезд'
+    end
+  end
+
+  def take_seat_or_volume
+    if !@trains.empty?
+      puts 'Выберите поезд'
+      index = 0
+      @trains.each { |value| puts "#{index += 1} - #{value.number} - #{value.type}" }
+      choice = gets.chomp.to_i
+      if choice <= @trains.size
+        if !@trains[choice - 1].quantity.empty?
+          puts 'Выберите вагон'
+          index = 0
+          if @trains[choice - 1].type == 'Грузовой'
+            @trains[choice - 1].show_wagons { |wagon| puts "#{index += 1} - #{wagon.type} вагон - объем: #{wagon.volume}" }
+            cargo_choice = gets.chomp.to_i
+            if cargo_choice <= @trains[choice - 1].quantity.size
+              print 'Укажите объем, который хотите занять: '
+              volume = gets.chomp.to_i
+              @trains[choice - 1].quantity[cargo_choice - 1].take_volume(volume)
+              @trains[choice - 1].quantity[cargo_choice - 1].free_volume
+            else
+              puts 'Нет такого вагона'
+            end
+          elsif @trains[choice - 1].type == 'Пассажирский'
+            @trains[choice - 1].show_wagons { |wagon| puts "#{index += 1} - #{wagon.type} вагон - мест: #{wagon.volume}" }
+            pass_choice = gets.chomp.to_i
+            if pass_choice <= @trains[choice - 1].quantity.size
+              @trains[choice - 1].quantity[pass_choice - 1].take_seat
+              @trains[choice - 1].quantity[pass_choice - 1].free_seats
+            else
+              puts 'Нет такого вагона'
+            end
+          end
+        else
+          puts 'Сначала прицепите вагоны'
+        end
+      else
+        puts 'Нет такого поезда'
+      end
+    else
+      puts 'Сначала создайте поезд'
+    end
+  end
+end
---
+++ Lesson8/train.rb
@@ -0,0 +1,130 @@
+require_relative 'company_name'
+require_relative 'instance_counter'
+
+class Train
+  include CompanyName
+  include InstanceCounter
+
+  NUMBER_FORMAT = /^[а-я0-9]{3}.*[а-я0-9]{2}$/i
+
+  attr_reader :number, :type, :speed, :quantity
+
+  @@trains = {}
+
+  def self.find(number)
+    puts @@trains[number]
+  end
+
+  def initialize(number, type)
+    @number = number
+    @type = type
+    @quantity = []
+    @speed = 0
+    validate!
+    @@trains[number] = self
+    register_instance
+    Train.instances
+    puts "Создан поезд #{@number}, тип #{@type}"
+  end
+
+  def valid?
+    validate!
+  rescue
+    false
+  end
+
+  def accelerate(speed)
+    @speed += speed
+    puts "Поезд разогнался на #{speed}"
+  end
+
+  def current_speed
+    puts "Текущая скорость #{@speed}"
+  end
+
+  def stop
+    puts 'Поезд остановился!'
+    @speed = 0
+  end
+
+  def add_wagon(wagon)
+    if speed_zero? && wagon.type == type
+      @quantity << wagon
+      puts "К поезду типа #{@type} добавлен вагон типа #{wagon.type}."
+      puts "Общее количество вагонов: #{@quantity.size}"
+    elsif wagon.type == type && !speed_zero?
+      puts 'Для начала остановите поезд!'
+    elsif !wagon.type == type
+      puts 'Нельзя присоединить данный вагон к этому типу поезда'
+    end
+  end
+
+  def remove_wagon
+    if !@quantity.empty?
+      if speed_zero?
+        @quantity.pop
+        puts "Вагон отцеплен. Общее количество вагонов: #{@quantity.size}"
+      else
+        puts 'Для начала остановите поезд!'
+      end
+    else
+      puts 'Для начала добавьте вагон'
+    end
+  end
+
+  def show_wagons
+    if !@quantity.empty?
+      puts 'Вагоны поезда:'
+      @quantity.each { |wagon| yield(wagon) }
+    else
+      puts 'У поезда нет вагонов'
+    end
+  end
+
+  def route=(route)
+    @route = route
+    @current_station = @route.stations[0]
+    puts "Поезд принял маршрут, текущая станция #{@current_station}"
+  end
+
+  def to_station(station)
+    if @route.stations.include?(station)
+      arr_index = @route.stations.index(station)
+      @current_station = @route.stations[arr_index]
+      puts "Поезд переместился на станцию #{@current_station}"
+    else
+      puts 'Такой станции нет!'
+    end
+  end
+
+  def current_station
+    puts "Текущая станция #{@current_station}"
+  end
+
+  def previous
+    arr_index = @route.stations.index(@current_station)
+    previous_station = @route.stations[arr_index - 1]
+    puts "Предыдущая станция #{previous_station}"
+  end
+
+  def next
+    arr_index = @route.stations.index(@current_station)
+    next_station = @route.stations[arr_index + 1]
+    puts "Следущая станция #{next_station}"
+  end
+
+  protected
+
+  def validate!
+    raise 'Номер не может быть nil!' if number.nil?
+    raise 'Номер должнен состоять минимум из 5 символов!' if number.to_s.length < 5
+    raise 'Невалидный номер!' if number !~ NUMBER_FORMAT
+    raise 'Тип поезда не может быть nil!' if type.nil?
+    raise "Тип поезда должен быть 'Грузовой' или 'Пассажирский'!" if type != 'Грузовой' && type != 'Пассажирский'
+    true
+  end
+
+  def speed_zero?
+    @speed.zero?
+  end
+end
