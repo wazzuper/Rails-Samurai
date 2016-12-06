efc939315cb0bd5411871b26f0d37667cb141410
artem <clifredd@gmail.com>
2016-12-06 23:00:56 +0500

    Lesson 9 fix2

--- Lesson9/accessors.rb
+++ Lesson9/accessors.rb
@@ -2,15 +2,17 @@ module Accessors
   def attr_accessor_with_history(*names)
     names.each do |name|
       var_name = "@#{name}".to_sym
-
+      
       define_method(name) { instance_variable_get(var_name) }
 
       define_method("#{name}=".to_sym) do |value|
         instance_variable_set(var_name, value)
-        instance_variable_set("@#{name}_history", eval("#{name}_history") << value)
+        @history_array ||= {}
+        @history_array[var_name] ||= []
+        @history_array[var_name] << value
       end
 
-      define_method("#{name}_history") { instance_variable_get("@#{name}_history") || [] }
+      define_method("#{name}_history") { @history_array[var_name] }
     end
   end
 
